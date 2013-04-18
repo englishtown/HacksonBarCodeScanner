d14699816c5f210fedc3e5a3aee3fa5fcfcd5937
Forrest <forrest.shi@ef.com>
2013-04-18 16:19:27 +0800

    Initial Commit

---
+++ TestBarcodeScanner/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestBarcodeScanner
+//
+//  Created by Shi Lin on 4/18/13.
+//  Copyright (c) 2013 Shi Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
